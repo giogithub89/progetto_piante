e55d2c6553ec419212daa2951e0f6c79a10f7ae9
baAlfioBattiato <battiato3@gmail.com>
2023-05-25 19:16:46 +0200

    modifica btn

--- src/pages/homepage/Homepage.js
+++ src/pages/homepage/Homepage.js
@@ -1,8 +1,8 @@
 import { Box } from "@mui/material";
 import React from "react";
-import ChiSiamo from "../../Component/homepage/ChiSiamo";
-import ComeFunziona from "../../Component/homepage/ComeFunziona";
-import SezImgaes from "../../Component/homepage/SezImgaes";
+import ChiSiamo from "../../Component/homepage/chiSiamo";
+import ComeFunziona from "../../Component/homepage/comeFunziona";
+import SezImgaes from "../../Component/homepage/sezImgaes";
 import Hero from "../homepage/Hero/index";
 
 const Homepage = () => {
