c2367635af28a796f285633e182f08da314f553f
giogithub89 <giobruno@me.com>
2023-05-30 16:30:22 +0200

    fix nomii

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,9 +1,9 @@
 import { Box } from "@mui/material";
 import React from "react";
-import ChiSiamo from "../Component/homepage/chiSiamo";
-import ComeFunziona from "../Component/homepage/comeFunziona";
-import SezImages from "../Component/homepage/sezImages";
-import Hero from "../Component/homepage/Hero/Hero";
+import ChiSiamo from "../Component/Homepage/chiSiamo";
+import ComeFunziona from "../Component/Homepage/comeFunziona";
+import SezImages from "../Component/Homepage/sezImages";
+import Hero from "../Component/Homepage/Hero/index";
 
 const Homepage = () => {
   return (
