659cf94f5ed846cf16373b488fd411fb210aa2cb
giogithub89 <giobruno@me.com>
2023-06-15 18:18:04 +0200

    map animals firebase

--- src/pages/Esplora/Esplora.jsx
+++ src/pages/Esplora/Esplora.jsx
@@ -1,9 +1,8 @@
 import SectionCategory from "../../Component/SectionCategory";
 import "./Esplora.css";
 import { db } from "../../config/firebase";
-import { getDocs } from "firebase/firestore";
-import { useState } from "react";
-import { useEffect } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { useState, useEffect } from "react";
 
 function Esplora() {
   const style = { "--i": 1 };
@@ -15,12 +14,43 @@ function Esplora() {
   const style7 = { "--i": 7 };
   const style8 = { "--i": 8 };
 
-  const [] = useState();
+  const [regions, setRegions] = useState([]);
+  const [cities, setCities] = useState([]);
+  const [animals, setAnimals] = useState([]);
+  const [places, setPlaces] = useState([]);
+  const [plants, setPlants] = useState([]);
+  const regionsCollection = collection(db, "regions");
+  const provincesCollection = collection(db, "provinces");
+  const citiesCollection = collection(
+    db,
+    "regions",
+    "HxI7dbXbKmCgdYeKy8TG",
+    "provinces",
+    "Qsxn8HKOl9BFxJfICl6x",
+    "cities"
+  );
 
   useEffect(() => {
     const getCat = async () => {
-      const data = await getDocs();
+      try {
+        const results = await getDocs(citiesCollection);
+        const data = results.docs.map((doc) => ({ ...doc.data(), id: doc.name }));
+        //console.log(data);
+        setCities(data);
+        data.forEach((city, index) => {
+          if (index === 2) {
+            console.log(city.animals.mammiferi);
+            setAnimals(city.animals.mammiferi);
+            setPlaces(city.places);
+            setPlaces(city.plants);
+            //const mammiferiImages = city.animals.mammiferi.map((animal) => animal.image);
+          }
+        });
+      } catch (err) {
+        console.log(err);
+      }
     };
+    getCat();
   }, []);
 
   return (
@@ -55,6 +85,15 @@ function Esplora() {
       <h2>Scegli la categoria :</h2>
 
       <SectionCategory />
+      <div>
+        <label htmlFor="label">Label</label>
+        {/* {cities.map((city, index) => (
+          <p>{city.name}</p>
+        ))} */}
+        {animals.map((category, index) => (
+          <p>{category.name}</p>
+        ))}
+      </div>
     </>
   );
 }
